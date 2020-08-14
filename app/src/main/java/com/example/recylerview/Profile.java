2db88a61e7e6d14b5466a536d10f946cd76bcde4
zubair <zubair1shareef@gmail.com>
2020-08-14 19:31:16 +0530

    14-8

--- app/src/main/java/com/example/recylerview/Profile.java
+++ app/src/main/java/com/example/recylerview/Profile.java
@@ -26,6 +26,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class Profile extends AppCompatActivity {
     TextView name;
@@ -69,6 +70,24 @@ public class Profile extends AppCompatActivity {
         //profileimage.setAnimation(mAnimation);
         //profileimage.setAnimation(animation);
        // profileimage.setVisibility(View.VISIBLE);
+        FirebaseUser userr = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = userr.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final DataAccess dataa = dataSnapshot.getValue(DataAccess.class);
+
+                Picasso.get().load(dataa.getImage()).into(profileimage);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
 
 
